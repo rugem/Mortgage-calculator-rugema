dbf42da510a1da8204f08df45faa345cf831ccc1
mclaud-pc <rugemam@g.cofc.edu>
2017-02-09 17:22:47 -0500

    Initial Commit

---
+++ app/src/main/java/edu/cofc/mortgage_calculator_rugema/MortgageSummaryActivity.java
@@ -0,0 +1,13 @@
+package edu.cofc.mortgage_calculator_rugema;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MortgageSummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mortgage_summary);
+    }
+}
